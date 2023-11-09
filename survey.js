697c48cce99d1a4560b93694500668b40ad4378c
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-09 10:11:35 -0500

    add completed paragraph to output to command line

--- survey.js
+++ survey.js
@@ -12,7 +12,7 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
         rl.question('What\'s your favourite thing to eat for that meal?', (food) => {
           rl.question('Which sport is your absolute favourite?', (sport) => {
             rl.question('What is your superpower? In a few words, tell us what you are amazing at!', (power) => {
-              console.log(name, activity, music, meal, food, sport, power);
+              console.log(`${name} listens to ${music} while ${activity}. They enjoy eating ${food} for ${meal}. Their favorite sport is ${sport}. If they had a superpower, it would be ${power}`);
 
               rl.close();
             });
